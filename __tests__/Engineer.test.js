86e1c935586b14d7f7004323512fec96501488a2
Eric Furukawa <eric.furukawa@gmail.com>
2021-12-04 11:34:30 -0800

    methods to Engineer.js

---
+++ __tests__/Engineer.test.js
@@ -0,0 +1,15 @@
+const Employee = require("../lib/Employee");
+const Engineer = require("../lib/Engineer.js");
+
+
+test("Can get Github ID via getGithub()", () => {
+    const testValue = "octocat";
+    const e = new Engineer("Foo", 1, "test@test.com", testValue);
+    expect(e.getGithub()).toBe(testValue);
+  });
+
+test("getRole() should return \"Engineer\"", () => {
+    const testValue = "Engineer";
+    const e = new Engineer("Foo", 1, "test@test.com");
+    expect(e.getRole()).toBe(testValue);
+  });
