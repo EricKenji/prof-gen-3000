8c39e4272881995a691113e4763593c9c545f1e0
Eric Furukawa <eric.furukawa@gmail.com>
2021-12-04 12:03:05 -0800

    added methods and passed test for Manager.js

---
+++ __tests__/Manager.test.js
@@ -0,0 +1,13 @@
+const Manager = require('../lib/Manager');
+
+test('Can get office number via getOffice()', () => {
+    const testValue = 2;
+    const e = new Manager("Foo", 1, 'test@test.com', testValue);
+    expect(e.getOffice()).toBe(testValue);
+});
+
+test('getRole() should return \"Manager"', () => {
+    const testValue = 'Manager';
+    const e = new Manager("Foo", 1, 'test@test.com');
+    expect(e.getRole()).toBe(testValue);
+});
