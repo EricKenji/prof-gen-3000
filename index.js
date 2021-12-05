3b02347d7f7a8029a8ce29eb95e463b44ee78292
Eric Furukawa <eric.furukawa@gmail.com>
2021-12-05 11:13:37 -0800

    added prompt for roles to index.js

--- index.js
+++ index.js
@@ -2,11 +2,12 @@ const inquirer = require('inquirer');
 const fs = require('fs');
 const generateHtml = require('./util/generateHtml')
 
-const questions = [
+const promptEmployee = () => {
+    return inquirer.prompt([
     {
         type: 'list',
         name: 'role',
-        message: "What type of employee is this?",
+        message: "What type of employee are you adding?",
         choices: ['Engineer', 'Intern', 'Manager']
     },
     {
@@ -14,20 +15,57 @@ const questions = [
         name: 'name',
         message: "What is the employee's name?"
     },
+    {
+        type: 'input',
+        name: 'id',
+        message: "What is the employee's ID number?"
+    },
+    {
+        type: 'input',
+        name: 'email',
+        message: "What is the employee's email address?"
+    },
 
-];
+
+]);
+};
+
+const promptRole = (promptEmployee) => {
+    let role = promptEmployee.role;
+    if (role === "Engineer") {
+        return inquirer.prompt([
+            {
+                type: 'input',
+                name: 'github',
+                message: "What is the engineer's GitHub ID?"
+            }
+        ])
+    }
+    if (role === "Intern") {
+        return inquirer.prompt([
+            {
+                type: 'input',
+                name: 'school',
+                message: "What school does this intern attend?"
+            }
+        ])
+    }
+    if (role === "Manager") {
+        return inquirer.prompt([
+            {
+                type: 'input',
+                name: 'officeNumber',
+                message: "What is the manager's office phone number?"
+            }
+        ])
+    };
+};
 
 function writeToFile(fileName, data) {
-    fs.writeFile(fileName, "data", err => {
+    fs.writeFile(fileName, data, err => {
         err ? console.log(err) : console.log('HTML file created')
     });
 };
 
-function init() {
-    inquirer.prompt(questions)
-    .then(function(data) {
-        writeToFile('team.html', generateHtml(data));
-    })
-};
-
-init();
+promptEmployee()
+.then(promptRole)
